02e665bed70a345482a9b03cd9621190d27c8983
Cole-Swiger <jcole598@aol.com>
2023-02-13 13:55:26 -0500

    Upset Birb: Initial commit to put on GitHub. Contains 2 levels and a main menu. This was created following a tutorial for the first level and second level created on my own.

---
+++ Assets/Birb.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Birb : MonoBehaviour
+{
+    private Vector3 _initialPosition;
+    private bool _birdLaunched;
+    private float _timeSitting;
+
+    [SerializeField] private float _launchSpeed = 500;
+
+    private void Awake() {
+        _initialPosition = transform.position;
+    }
+
+    private void Update() {
+        GetComponent<LineRenderer>().SetPosition(0, _initialPosition);
+        GetComponent<LineRenderer>().SetPosition(1, transform.position);
+
+        if (_birdLaunched && GetComponent<Rigidbody2D>().velocity.magnitude <= 0.1) {
+            _timeSitting += Time.deltaTime;
+        }
+
+        if (transform.position.y > 20 ||
+            transform.position.y < -10 ||
+            transform.position.x > 20 ||
+            transform.position.x < -20 ||
+            _timeSitting >= 3) 
+        {
+            String currentScene = SceneManager.GetActiveScene().name;
+            SceneManager.LoadScene(currentScene);
+        }
+    }
+
+    private void OnMouseDown() {
+        GetComponent<SpriteRenderer>().color = Color.red;
+        GetComponent<LineRenderer>().enabled = true;
+    }
+
+    private void OnMouseUp() {
+        GetComponent<SpriteRenderer>().color = Color.white;
+
+        Vector2 directionToInitialPosition = _initialPosition - transform.position;
+        GetComponent<Rigidbody2D>().AddForce(directionToInitialPosition * _launchSpeed);
+        GetComponent<Rigidbody2D>().gravityScale = 1;
+        _birdLaunched = true;
+
+        GetComponent<LineRenderer>().enabled = false;
+    }
+
+    private void OnMouseDrag() {
+        Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(newPosition.x, newPosition.y);
+    }
+}
---
+++ Assets/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private GameObject _dustPrefab;
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.collider.GetComponent<Birb>() != null) {
+            Instantiate(_dustPrefab, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+            return;
+        }
+
+        Enemy enemy = collision.collider.GetComponent<Enemy>();
+        if (enemy != null) {
+            return;
+        }
+
+        if (collision.contacts[0].normal.y < -0.5) {
+            Instantiate(_dustPrefab, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
